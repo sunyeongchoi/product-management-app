724702be3ad4bb6acc56802bda2f8fc3839193ba
sunyeongchoi <suoung0716@gmail.com>
2024-02-21 22:50:44 +0900

    [product] product api
    * product API 구현 프로토타입

--- cmd/product/product.go
+++ cmd/product/product.go
@@ -1,1 +1,131 @@
-package cmd
+package product
+
+import (
+	"log"
+	"net/http"
+	"product-management/models"
+	products "product-management/sql/product"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type apiManager struct{}
+
+func GetProductAPIManager() *apiManager {
+	return &apiManager{}
+}
+
+func (p apiManager) Register(c *gin.Context) {
+	var product models.Product
+	if err := c.ShouldBindJSON(&product); err != nil {
+		log.Println("err: ", err)
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+	if product.Size != "small" && product.Size != "large" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "잘못된 상품 사이즈 입니다.",
+		})
+		return
+	}
+	err := products.Register(product)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "error",
+			"data":    err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+		"data":    product,
+	})
+}
+
+func (p apiManager) Update(c *gin.Context) {
+	id := c.Param("id")
+	idStr, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "error",
+			"data":    err.Error(),
+		})
+		return
+	}
+	var updateFields map[string]interface{}
+	if err := c.ShouldBindJSON(&updateFields); err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+	err = products.Update(idStr, updateFields)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "error",
+			"data":    err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+	})
+}
+
+func (p apiManager) List(c *gin.Context) {
+	list, err := products.List()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "error",
+			"data":    err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+		"data":    list,
+	})
+}
+
+func (p apiManager) Get(c *gin.Context) {
+	id := c.Param("id")
+	product, err := products.Get(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "error",
+			"data":    err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+		"data":    product,
+	})
+}
+
+func (p apiManager) Delete(c *gin.Context) {
+	id := c.Param("id")
+	err := products.Delete(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "error",
+			"data":    err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+	})
+}
+
+func (p apiManager) Search(c *gin.Context) {
+	list, err := products.Search()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "error",
+			"data":    err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+		"data":    list,
+	})
+}
--- sql/product/product.go
+++ sql/product/product.go
@@ -1,23 +1,112 @@
 package product
 
 import (
-	"fmt"
+	"database/sql"
+	"errors"
 	"product-management/models"
 	mysql "product-management/sql"
+	"time"
 )
 
 func Register(product models.Product) error {
-	fmt.Println(product)
 	query := "INSERT INTO product (manager_id, category, price, `name`, description, `size`, expired_date) values (?, ?, ?, ?, ?, ?, ?)"
 	_, err := mysql.DBConn.Exec(query, product.ManagerID, product.Category, product.Price, product.Name, product.Description, product.Size, product.ExpiredDate)
 	if err != nil {
-		fmt.Println("error!!!!!!!!", err)
 		return err
 	}
 	return nil
-	//db.ExecContext();
-	//db.QueryContext();
-	//db.QueryRowContext();
-	//db.PrepareContext();
 }
 
+func Update(id int, updateFields map[string]interface{}) error {
+	query := "UPDATE product SET "
+	var params []interface{}
+	var err error
+	for key, value := range updateFields {
+		query += key + "=?, "
+		if key == "expired_date" {
+			value, err = time.Parse(time.RFC3339, value.(string))
+			if err != nil {
+				return err
+			}
+		}
+		params = append(params, value)
+	}
+	query = query[:len(query)-2] + " WHERE id = ?"
+	params = append(params, id)
+	_, err = mysql.DBConn.Exec(query, params...)
+	return err
+}
+
+func List() ([]models.Product, error) {
+	var products []models.Product
+	query := "SELECT id, manager_id, category, price, name, description, size, expired_date FROM product"
+	rows, err := mysql.DBConn.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	// TODO: 페이지네이션 - cursor based pagination 기반으로, 1page 당 10개의 상품이 보이도록
+	for rows.Next() {
+		var product models.Product
+		if err := rows.Scan(&product.ID, &product.ManagerID, &product.Category, &product.Price, &product.Name, &product.Description, &product.Size, &product.ExpiredDate); err != nil {
+			return nil, err
+		}
+		products = append(products, product)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func Get(id string) (models.Product, error) {
+	var product models.Product
+	query := "SELECT id, manager_id, category, price, name, description, size, expired_date FROM product WHERE id = ?"
+	row := mysql.DBConn.QueryRow(query, id)
+	if err := row.Scan(&product.ID, &product.ManagerID, &product.Category, &product.Price, &product.Name, &product.Description, &product.Size, &product.ExpiredDate); err != nil {
+		if err == sql.ErrNoRows {
+			return models.Product{}, errors.New("Product not found")
+		}
+		return models.Product{}, err
+	}
+	return product, nil
+}
+
+func Delete(id string) error {
+	query := "DELETE FROM product WHERE id = ?"
+	result, err := mysql.DBConn.Exec(query, id)
+	if err != nil {
+		return err
+	}
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected == 0 {
+		return errors.New("No rows affected. Product not found.")
+	}
+	return nil
+}
+
+// TODO: 검색 (초성검색, 단어검색) - 예) 슈크림 라떼 → 검색가능한 키워드 : 슈크림, 크림, 라떼, ㅅㅋㄹ, ㄹㄸ
+func Search() ([]models.Product, error) {
+	var products []models.Product
+	query := "SELECT id, manager_id, category, price, name, description, size, expired_date FROM product"
+	rows, err := mysql.DBConn.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	// TODO: 페이지네이션
+	for rows.Next() {
+		var product models.Product
+		if err := rows.Scan(&product.ID, &product.ManagerID, &product.Category, &product.Price, &product.Name, &product.Description, &product.Size, &product.ExpiredDate); err != nil {
+			return nil, err
+		}
+		products = append(products, product)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return products, nil
+}
