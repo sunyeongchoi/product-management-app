e6fedd9b056f4e3d422d30105b385007dbab0e0c
sunyeongchoi <suoung0716@gmail.com>
2024-02-21 22:48:46 +0900

    [product] product model
    * product 구조체 선언

--- models/product.go
+++ models/product.go
@@ -1,1 +1,14 @@
 package models
+
+import "time"
+
+type Product struct {
+	ID          int 		`json:"id" db:"id"`
+	ManagerID   int 		`json:"manager_id" db:"manager_id"`
+	Category    string 		`json:"category" db:"category"`
+	Price       string 		`json:"price" db:"price"`
+	Name        string 		`json:"name" db:"name"`
+	Description string 		`json:"description" db:"description"`
+	Size        string 		`json:"size" db:"size"` // small or large
+	ExpiredDate time.Time 	`json:"expired_date" db:"expired_date"`
+}
