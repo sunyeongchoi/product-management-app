0dca63b8225897b24fe540b30389787d0cf1cc19
sunyeongchoi <suoung0716@gmail.com>
2024-02-21 22:51:59 +0900

    [common] router setting and database config setting
    * product routing rule 정의
    * database connection 함수 호출

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	mysql "database/sql"
+	"github.com/gin-gonic/gin"
+	"log"
+	"product-management/cmd/product"
+	"product-management/sql"
+)
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
+	r.POST("/product", product.GetProductAPIManager().Register)
+	r.PATCH("/product/:id", product.GetProductAPIManager().Update)
+	r.GET("/products", product.GetProductAPIManager().List)
+	r.GET("/product/:id", product.GetProductAPIManager().Get)
+	r.DELETE("/product/:id", product.GetProductAPIManager().Delete)
 	return r
 }
 
 func main() {
 	r := setupRouter()
+	sql.ConnectToDB()
+	defer func(dbConn *mysql.DB) {
+		err := dbConn.Close()
+		if err != nil {
+			log.Fatal("error from close database connection")
+		}
+	}(sql.DBConn)
 	r.Run(":8080")
 }
